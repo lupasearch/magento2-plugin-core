405432dac0545837a0ae3bcb60b6bf4028415f5b
aurzal <aurimas@lupasearch.com>
2025-10-30 16:48:03 +0200

    register php client directory as component

--- registration.php
+++ registration.php
@@ -5,3 +5,11 @@ declare(strict_types=1);
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'LupaSearch_LupaSearchPluginCore', __DIR__);
+ComponentRegistrar::register(
+    ComponentRegistrar::LIBRARY,
+    'lupasearch/lupasearch-php-client',
+    __DIR__ . DIRECTORY_SEPARATOR
+    . '..' . DIRECTORY_SEPARATOR
+    . 'lupasearch-php-client' . DIRECTORY_SEPARATOR
+    . 'src',
+);
